6322a81df3ed1ccc0a9aa3795273254a190fd150
Taritree <taritree.wongjirad@gmail.com>
2018-06-25 15:16:22 -0400

    more civilized setting of pythonpath

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-export PYTHONPATH=$PWD:${PYTHONPATH}
+# setups environment variables for larcvdataset, an additional wrapper to larcv::ThreadFiller
+# assumes script called in the folder code sits
+
+# clean up previously set env
+if [[ -z $FORCE_LARCVDATASET_BASEDIR ]]; then
+    where="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    export LARCVDATASET_BASEDIR=${where}
+else
+    export LARCVDATASET_BASEDIR=$FORCE_LARCVDATASET_BASEDIR
+fi
+
+# Add to python path
+[[ ":$PYTHONPATH:" != *":${LARCVDATASET_BASEDIR}:"* ]] && PYTHONPATH="${LARCVDATASET_BASEDIR}:${PYTHONPATH}"
